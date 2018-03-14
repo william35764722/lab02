f9c0c6748ed02a95a23efd8180c9e16ea9c37ad7
william35764722 <34229178+william35764722@users.noreply.github.com>
2018-03-14 15:37:27 +0800

    Add files via upload

---
+++ 04050724/04050724.ino
@@ -0,0 +1,41 @@
+const byte NUM=8;
+const byte
+led[]={3,4,5,8,9,10,11,12};
+const int delaytime=50;
+int ii;
+void setup() {
+for (ii=0;ii<NUM;ii++)
+{ pinMode(led[ii],OUTPUT);
+ digitalWrite(led[ii],LOW);
+}
+}
+
+
+void loop()
+/*左移*/
+ {
+for(ii=0;ii<NUM;ii++)
+
+{ digitalWrite(led[ii],HIGH);
+ delay(delaytime);
+ digitalWrite(led[ii],LOW);
+ }
+ for (ii=0;ii<NUM;ii++)
+{
+ digitalWrite(led[ii],LOW);
+}
+delay(delaytime);
+
+/*右移*/
+ for(ii=NUM-1;ii>=0;ii--)
+
+{ digitalWrite(led[ii],HIGH);
+ delay(delaytime);
+ digitalWrite(led[ii],LOW);
+ }
+ for (ii=0;ii<NUM;ii++)
+{
+ digitalWrite(led[ii],LOW);
+}
+delay(delaytime);
+}
